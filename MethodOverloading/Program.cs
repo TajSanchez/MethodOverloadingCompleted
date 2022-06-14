37d2242ea4ab487e7ea8f665d0786935fef73a23
Taj <tajmarie92@gmail.com>
2022-06-13 21:02:29 -0500

    Add project files.

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        //public class Point
+        //{
+        //    public void Move(int x, int y) { }
+
+        //    public void Move(Point newLocation) { }
+
+        //    public void Move(Point newLocation, int speed) { }
+        //}
+
+        //public class Calculator
+        //{
+        //    public int Add(params int[] numbers) {}
+
+        //}
+        //var result =Calculator.Add(new int[] {1,2,3,4});
+        //var result = Calculator.Add(1, 2, 3, 4);
+
+
+         
+    }
+}
